7eba21d1ee836af66a90281006975d55605c8e6f
hard1mpulse <linkup811@gmail.com>
2020-04-22 07:33:25 -0500

    Выполнено 7.2b

---
+++ tasks/Par7/7.2b.py
@@ -0,0 +1,19 @@
+from sys import argv
+path_to_file='Par7_files/'+argv[1]
+ignore = ['duplex', 'alias', 'Current configuration']
+skip_line=False
+result_file_name=argv[1][0:(argv[1].find('.'))]+'_cleared.txt'
+result=open('Par7_files/'+result_file_name,'w')
+with open(path_to_file,'r') as file:
+    for line in file:
+        for i in ignore:
+            if i in line:
+                skip_line=True
+                break
+            else:
+                skip_line=False
+        if skip_line:
+            pass
+        else:
+            result.write(line)
+result.close()
